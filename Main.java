ad7487f77cc509f9b637bc413d2fd8d1d0245052
Winaldi99 <2381030@unai.edu>
2024-11-14 10:08:49 +0700

    Commit

--- Main.java
+++ Main.java
@@ -4,9 +4,13 @@ import services.TodoListService;
 import services.TodoListServiceImpl;
 import views.TodoListTerminalView;
 import views.TodoListView;
+import config.Database;
 
 public class Main {
     public static void main(String[] args) {
+        Database database = new Database("my_database", "root", "", "localhost", "3306");
+        database.setup();
+
         TodoListRepository todoListRepository = new TodoListRepositoryImpl();
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
         TodoListView todoListView = new TodoListTerminalView(todoListService);
